2d2e1be907454b83cb20d6107232503d62266ff6
PedroRF123154 <info@seogirona.es>
2026-03-03 10:11:06 +0100

    Update SignatureCapture.js

--- www/SignatureCapture.js
+++ www/SignatureCapture.js
@@ -1,7 +1,11 @@
 cordova.define("es.seogirona.signaturecapture.SignatureCapture", function(require, exports, module) {
-var exec = require('cordova/exec');
+  var exec = require('cordova/exec');
 
-exports.captureSignature = function(success, error) {
-    exec(success, error, "SignatureCapture", "captureSignature", []);
-};
+  exports.openSignatureScreen = function (success, error) {
+    exec(success, error, "SignatureCapture", "openSignatureScreen", []);
+  };
+
+  exports.captureSignature = function (success, error) {
+    exec(success, error, "SignatureCapture", "openSignatureScreen", []);
+  };
 });
